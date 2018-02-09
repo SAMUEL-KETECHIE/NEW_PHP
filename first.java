b0780c0d70214144281bb0136d1abc71c503b636
SAMUEL KETECHIE <samuelwendolin2@gmail.com>
2018-02-09 14:56:03 -0800

    adding java file

---
+++ first.java
@@ -0,0 +1,16 @@
+package com.wensoft;
+
+/*
+Copyright 2018, WenSoft Inc Gh.
+By Samuel Wendolin Ketechie
+This is a simple project to show my git and github capability/ability to use the platform 
+
+*/
+public class first{
+
+public static void main(String[] args){
+	
+}
+
+
+}
